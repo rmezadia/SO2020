f78d76d939ff7fec249bf9f8edcba135b05ecba3
Ricardo José Meza Díaz <rmezadia@alumnos.unex.es>
2020-10-04 13:04:32 +0200

    Practica 1 buscaminas
    Se ha realizado el tablero, solo falta adecuar bien las celdas para ajustarlas a los marcos.

--- buscaminacum/buscaminascum.sh
+++ buscaminacum/buscaminascum.sh
@@ -44,6 +44,7 @@ function volverAtras(){
  		case $var in
      		1)
        			echo "opcion 1"
+            creaTablero
      			;;
      		2)
       			echo "opcion 2"
@@ -95,6 +96,66 @@ function menuUntil(){
   	
 }
 
+#-------------------------------FUNCIONES TABLERO---------------------
+
+
+#Pinta matriz 
+function creaTablero(){
+
+  #Contador de puntos
+  puntos = 0
+
+ #Celdas aleatorias 
+  a = "1 10-10 -1 "
+  b = "-1 0 1 "
+  c = "0 1 "
+  d = "-1 0 1-2-3 "
+  e = "1 2 20 21 10 0-10-20 - 23-2-1 "
+  f = "1 2 3 35 30 22 10 0-10-20-25-30-35-3-2-1 "
+  g = "1 4 6 9 10 15 20 25 30-30 - 24-11-10-9-8-7 "
+
+  #Variable celda almacena el valor de cada celda
+  declare -a matriz
+
+  contador = 0
+
+  #Encabezado de columnas con espaciado
+  printf '\n\n'
+  printf '%s' "      a   b   c   d   e  f  g   h   i   j"
+  printf '\n    %s\n' "----------------------------------------"
+
+  
+
+  #Imprime filas del 0 al 9 
+  for fila in $(seq 0 9); do 
+    printf '%d ' "$fila"
+
+    #imprime columnas en base a filas
+    for columna in $(seq 0 9); do
+      ((contador+=1))
+      creaCeldaVacia $contador 
+      printf '%s \e[33m%s\e[0m ' "|" "${matriz[$contador]}"
+    done
+
+    printf '%s\n' "|"
+    printf '   %s\n' "----------------------------------------"
+  done
+
+  printf '\n\n' 
+
+
+}
+
+#Celda vacia
+function creaCeldaVacia(){
+
+  local contador2=$1
+    if [[ -z "${matriz[$contador2]}" ]]; then
+      matriz[$contador]="."
+    fi
+}
+
+#voy por metodo is_null_field() inclusive
 
 #------------------------------------MAIN----------------------------------
 
